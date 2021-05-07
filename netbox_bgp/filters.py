a13407c107516183c96da91a07b6d3f2eeafc3e2
Omri Abu <omri.abu@ice.com>
2021-05-07 16:23:34 -0400

    Added new filter fields to BGPSessionFilterSet for local_address, remote_address, device, and name. Added object name/id filter options to device field. Added object number/id filter options to remote_as, and local_as fields. Added object address/id filter options to remote_address and local_address fields.

--- netbox_bgp/filters.py
+++ netbox_bgp/filters.py
@@ -4,6 +4,8 @@ from django.db.models import Q
 from utilities.filters import TagFilter
 
 from .models import ASN, Community, BGPSession, RoutingPolicy
+from ipam.models import IPAddress
+from dcim.models import Device
 
 
 class ASNFilterSet(django_filters.FilterSet):
@@ -22,9 +24,9 @@ class ASNFilterSet(django_filters.FilterSet):
         if not value.strip():
             return queryset
         qs_filter = (
-            Q(id__icontains=value)
-            | Q(number__icontains=value)
-            | Q(description__icontains=value)
+                Q(id__icontains=value)
+                | Q(number__icontains=value)
+                | Q(description__icontains=value)
         )
         return queryset.filter(qs_filter)
 
@@ -45,9 +47,9 @@ class CommunityFilterSet(django_filters.FilterSet):
         if not value.strip():
             return queryset
         qs_filter = (
-            Q(id__icontains=value)
-            | Q(value__icontains=value)
-            | Q(description__icontains=value)
+                Q(id__icontains=value)
+                | Q(value__icontains=value)
+                | Q(description__icontains=value)
         )
         return queryset.filter(qs_filter)
 
@@ -58,33 +60,88 @@ class BGPSessionFilterSet(django_filters.FilterSet):
         label='Search',
     )
     tag = TagFilter()
+
     remote_as = django_filters.ModelMultipleChoiceFilter(
+        field_name='remote_as__number',
         queryset=ASN.objects.all(),
+        to_field_name='number',
+        label='Remote AS (Number)',
+    )
+    remote_as_id = django_filters.ModelMultipleChoiceFilter(
+        field_name='remote_as__id',
+        queryset=ASN.objects.all(),
+        to_field_name='id',
+        label='Remote AS (ID)',
     )
     local_as = django_filters.ModelMultipleChoiceFilter(
+        field_name='local_as__number',
         queryset=ASN.objects.all(),
+        to_field_name='number',
+        label='Local AS (Number)',
+    )
+    local_as_id = django_filters.ModelMultipleChoiceFilter(
+        field_name='local_as__id',
+        queryset=ASN.objects.all(),
+        to_field_name='id',
+        label='Local AS (ID)',
     )
     import_policies = django_filters.ModelMultipleChoiceFilter(
-
         queryset=RoutingPolicy.objects.all(),
     )
     export_policies = django_filters.ModelMultipleChoiceFilter(
         queryset=RoutingPolicy.objects.all(),
     )
+    local_address_id = django_filters.ModelMultipleChoiceFilter(
+        field_name='local_address__address_id',
+        queryset=IPAddress.objects.all(),
+        to_field_name='id',
+        label='Local Address (ID)',
+    )
+    local_address = django_filters.ModelMultipleChoiceFilter(
+        field_name='local_address__address',
+        queryset=IPAddress.objects.all(),
+        to_field_name='address',
+        label='Local Address',
+    )
+    remote_address_id = django_filters.ModelMultipleChoiceFilter(
+        field_name='remote_address__address_id',
+        queryset=IPAddress.objects.all(),
+        to_field_name='id',
+        label='Remote Address (ID)',
+    )
+    remote_address = django_filters.ModelMultipleChoiceFilter(
+        field_name='remote_address__address',
+        queryset=IPAddress.objects.all(),
+        to_field_name='address',
+        label='Remote Address',
+    )
+
+    device_id = django_filters.ModelMultipleChoiceFilter(
+        field_name='device__id',
+        queryset=Device.objects.all(),
+        to_field_name='id',
+        label='Device (ID)',
+    )
+    device = django_filters.ModelMultipleChoiceFilter(
+        field_name='device__name',
+        queryset=Device.objects.all(),
+        to_field_name='name',
+        label='Device (name)',
+    )
 
     class Meta:
         model = BGPSession
-        fields = ['description', 'status', 'tenant']
+        fields = ['name', 'description', 'status', 'tenant']
 
     def search(self, queryset, name, value):
         """Perform the filtered search."""
         if not value.strip():
             return queryset
         qs_filter = (
-            Q(remote_as__number__icontains=value)
-            | Q(name__icontains=value)
-            | Q(local_as__number__icontains=value)
-            | Q(description__icontains=value)
+                Q(remote_as__number__icontains=value)
+                | Q(name__icontains=value)
+                | Q(local_as__number__icontains=value)
+                | Q(description__icontains=value)
         )
         return queryset.filter(qs_filter)
 
@@ -105,7 +162,7 @@ class RoutingPolicyFilterSet(django_filters.FilterSet):
         if not value.strip():
             return queryset
         qs_filter = (
-            Q(name__icontains=value)
-            | Q(description__icontains=value)
+                Q(name__icontains=value)
+                | Q(description__icontains=value)
         )
         return queryset.filter(qs_filter)
