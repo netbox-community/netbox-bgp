479dfffbac4f0156fd8f869ebf201acb59047ac1
Юзефович Николай Игоревич <uni@skbkontur.ru>
2021-11-16 10:49:09 +0500

    add supp for nb3.0.10;

--- netbox_bgp/__init__.py
+++ netbox_bgp/__init__.py
@@ -12,7 +12,7 @@ class BGPConfig(PluginConfig):
     base_url = 'bgp'
     required_settings = []
     min_version = '3.0.0'
-    max_version = '3.0.8'
+    max_version = '3.0.10'
     default_settings = {
         'device_ext_page': 'right',
         'asdot': False
