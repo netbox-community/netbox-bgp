013d264a7c6b3ab6cd5c5929d0db7e11d7fed673
Jan Krupa <jan.krupa@cesnet.cz>
2025-08-27 08:46:02 +0000

    Refactor device extension to use HTMX table instead of Django tables
    - Remove manual BGPSession filtering and table creation from template_content.py
    - Replace render_table with htmx_table in device_extend.html template
    - Simplify x_page() method by removing extra_context parameter
    - Clean up unused

--- netbox_bgp/template_content.py
+++ netbox_bgp/template_content.py
@@ -1,9 +1,5 @@
 from netbox.plugins import PluginTemplateExtension
 
-from .models import BGPSession
-from .tables import BGPSessionTable
-
-
 class DeviceBGPSession(PluginTemplateExtension):
     models = ('dcim.device',)
 
@@ -23,14 +19,8 @@ class DeviceBGPSession(PluginTemplateExtension):
         return ''
 
     def x_page(self):
-        obj = self.context['object']
-        sess = BGPSession.objects.filter(device=obj)
-        sess_table = BGPSessionTable(sess)
         return self.render(
             'netbox_bgp/device_extend.html',
-            extra_context={
-                'related_session_table': sess_table
-            }
         )
 
 template_extensions = [DeviceBGPSession]
