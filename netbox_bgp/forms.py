615f86a1c911a8d76c12d4939e4edc1e839e6b40
Etienne BRUNEL <etienne.brunel@outlook.com>
2026-01-21 10:38:09 +0000

    add: Add max_prefixes on BGPSessionImportForm and BGPSessionBulkEditForm

--- netbox_bgp/forms.py
+++ netbox_bgp/forms.py
@@ -276,7 +276,7 @@ class BGPSessionForm(NetBoxModelForm):
         ),
     )
     max_prefixes = forms.IntegerField(
-        label="Maximum Prefixes",
+        label=_("Maximum Prefixes"),
         required=False,
         min_value=1,
     )
@@ -449,6 +449,7 @@ class BGPSessionImportForm(NetBoxModelImportForm):
             "tags",
             "prefix_list_in",
             "prefix_list_out",
+            "max_prefixes",
         ]
 
 
@@ -551,6 +552,11 @@ class BGPSessionBulkEditForm(NetBoxModelBulkEditForm):
         required=False,
         widget=APISelectMultiple(api_url="/api/plugins/bgp/routing-policy/"),
     )
+    max_prefixes = forms.IntegerField(
+        label=_("Maximum Prefixes"),
+        required=False,
+        min_value=1,
+    )
 
     model = BGPSession
 
@@ -570,7 +576,7 @@ class BGPSessionBulkEditForm(NetBoxModelBulkEditForm):
         FieldSet("remote_as", "remote_address", name="Remote"),
         FieldSet("local_as", "local_address", name="Local"),
         FieldSet("import_policies", "export_policies", name="Policies"),
-        FieldSet("prefix_list_in", "prefix_list_out", name="Prefixes"),
+        FieldSet("max_prefixes", "prefix_list_in", "prefix_list_out", name="Prefixes"),
     )
 
     nullable_fields = [
